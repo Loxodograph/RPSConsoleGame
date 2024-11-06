7fa656238bf19dfffae71b9d54a1d3d354990de6
Loxodograph <paldino.nicholas@gmail.com>
2024-11-06 09:56:15 -0500

    Implemented suggested changes.

--- RPSGame.js
+++ RPSGame.js
@@ -2,8 +2,9 @@ const READLINE = require('readline-sync');
 
 const VALID_CHOICES = ['rock', 'scissors', 'paper', 'lizard', 'Spock'];
 const SHORTENED_VALID_CHOICES = ['r', 's', 'p', 'l', 'S'];
+const MAX_ROUNDS = 5;
 
-const CHOICES = {
+const WINNING_COMBOS = {
   r: ["scissors", "lizard"],
   s: ["paper", "lizard"],
   p: ["rock", "Spock"],
@@ -11,7 +12,6 @@ const CHOICES = {
   S: ["rock", "scissors"],
 }
 
-let choice;
 let gameCount = 1;
 let playerScore = 0;
 let computerScore = 0;
@@ -35,19 +35,22 @@ while (true) {
     answer = READLINE.question().toLowerCase();
   }
 
-  if (answer[0] === 'y') {
+  if (answer === 'y' || answer.toLowerCase() === "yes" || answer.toLowerCase() === "yass") {
+    console.clear();
     gameCount = 1;
     playerScore = 0;
     computerScore = 0;
-  } else if (answer[0] === "n") {
+  } else if (answer !== "y" && answer.toLowerCase() !== "yes" || answer.toLowerCase() !== "yass") {
+    console.clear();
     prompt("Goodbye and Thanks for Playing");
     break;
   };
 }
 
 function roundLoop() {
-  while (gameCount <= 5) {
+  while (gameCount <= MAX_ROUNDS) {
     gameRound();
+    gameCount += 1;
   }
   if (playerScore > computerScore) {
   prompt(`Player Score: ${playerScore} Computer Score: ${computerScore}`);
@@ -65,36 +68,39 @@ function gameRound() {
   let computerChoice = VALID_CHOICES[generateRandomNumber()];
   prompt("---------");
   prompt(`Round ${gameCount}`);
-  userChoicePrompt();
+  let choice = userChoicePrompt();
   isValidChoice(choice);
-  validateInput(choice);
-  roundPlay(choice, computerChoice);
+  let validatedChoice = validateInput(choice);
+  roundPlay(validatedChoice, computerChoice);
 }
 
 function validateInput(chosenOption) {
   if (chosenOption.length === 1) {
-    prompt(`You chose ${VALID_CHOICES[SHORTENED_VALID_CHOICES.indexOf(chosenOption.slice(0,1))]}`);
-    choice = VALID_CHOICES[SHORTENED_VALID_CHOICES.indexOf(chosenOption.slice(0,1))]
+    prompt(`You chose ${VALID_CHOICES[SHORTENED_VALID_CHOICES.indexOf(chosenOption.slice(0, 1))]}`);
+    chosenOption = VALID_CHOICES[SHORTENED_VALID_CHOICES.indexOf(chosenOption.slice(0, 1))]
   } else {
     prompt(`You chose ${chosenOption}`)
   }
+  return chosenOption;
 }
 
 function isValidChoice(chosenOption) {
+  if (chosenOption === "spock") {
+    chosenOption = "Spock"
+  };
   while ((!VALID_CHOICES.includes(chosenOption)) && (!SHORTENED_VALID_CHOICES.includes(chosenOption.slice(0, 1)))) {
     console.log("Try Again");
     prompt('Choose one: (r)ock, (p)aper, (s)cissors, (l)izard, (S)pock');
     chosenOption = READLINE.question();
   }
-  choice = chosenOption;
-  return;
+  return chosenOption;
 };
 
 function userChoicePrompt() {
   prompt("");
   prompt("");
   prompt('Choose one: (r)ock, (p)aper, (s)cissors, (l)izard, (S)pock');
-  choice = READLINE.question();
+  let choice = READLINE.question();
   prompt("");
   return choice;
 }
@@ -103,16 +109,16 @@ function roundPlay(chosenOption, chosenComputerOption) {
   if (chosenOption === chosenComputerOption) {
     prompt(`Computer chose ${chosenComputerOption}`)
     prompt('Draw');
-  } else if (CHOICES[chosenOption.slice(0,1)].includes(chosenComputerOption)) {
+  } else if (WINNING_COMBOS[chosenOption.slice(0, 1)].includes(chosenComputerOption)) {
     playerScore += 1;
     prompt(`Computer chose ${chosenComputerOption}`);
     prompt('You Win!');
-  } else if (!CHOICES[chosenOption.slice(0,1)].includes(chosenComputerOption)) {
+  } else {
     computerScore += 1;
     prompt(`Computer chose ${chosenComputerOption}`);
     prompt('You Lose!');
   }
-  gameCount += 1;
+  
   prompt(`Your Score ${playerScore}`);
   prompt(`Computer Score ${computerScore}`);
 }
